87dab54de61de449417fcd31a67590f893da7cb3
nathanmaru <nathanmaru7@gmail.com>
2021-11-11 23:25:27 +0800

    bring back settings

--- core/settings.py
+++ core/settings.py
@@ -107,11 +107,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        # "NAME": env("DATABASE_NAME"),
-        # "USER": env("DATABASE_USER"),
-        # "PASSWORD": env("DATABASE_PASSWORD"),
-        # "HOST": "localhost",
-        # "PORT": "5432",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 import dj_database_url
