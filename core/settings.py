0f61c94069b08289563e4c967986f1b4c1756e02
nathanmaru <nathanmaru7@gmail.com>
2021-11-11 22:31:17 +0800

    update database

--- core/settings.py
+++ core/settings.py
@@ -106,10 +106,14 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
