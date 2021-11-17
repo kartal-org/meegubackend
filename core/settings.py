062b0076e8204dd5d8d31ac8ede23b6ee4fd33f5
nathanmaru <55788297+nathanmaru@users.noreply.github.com>
2021-11-17 12:59:18 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -125,10 +125,10 @@ DATABASES = {
 DATABASES["default"] = DATABASES["dev" if DEBUG else "production"]
 
 
-import dj_database_url
+# import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES["default"].update(db_from_env)
 
 
 # Password validation
